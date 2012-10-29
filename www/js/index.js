4fedaabdb7c0507152a5e1dc9d541569c41f6031
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2012-10-29 09:24:47 -0600

    Done with internet

--- www/js/index.js
+++ www/js/index.js
@@ -675,3 +675,33 @@ function tryPassword(){
         $('#security #result').css('color', 'red');
     };
 }
+
+// Internet
+
+$(document).delegate('#internet', 'pageshow', function () {
+    $.getJSON('https://graph.facebook.com/cocacola', function(data) {
+        $('#internet #facebook').text(JSON.stringify(data));
+    });
+});
+
+// implement JSON.stringify serialization
+JSON.stringify = JSON.stringify || function (obj) {
+    var t = typeof (obj);
+    if (t != "object" || obj === null) {
+        // simple data type
+        if (t == "string") obj = '"'+obj+'"';
+        return String(obj);
+    }
+    else {
+        // recurse array or object
+        var n, v, json = [], arr = (obj && obj.constructor == Array);
+        for (n in obj) {
+            v = obj[n]; t = typeof(v);
+            if (t == "string") v = '"'+v+'"';
+            else if (t == "object" && v !== null) v = JSON.stringify(v);
+            json.push((arr ? "" : '"' + n + '":') + String(v));
+        }
+        return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+    }
+};
+
